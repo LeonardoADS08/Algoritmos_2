f58b4b95ecc665c1590bc5cb06b166eb9c52c470
Leonardo Arteaga dos Santos <LeonardoADS08@gmail.com>
2017-05-15 21:21:22 -0400

    Update Interfaz_Examen.cpp

--- Interfaz_Examen.cpp
+++ Interfaz_Examen.cpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "Arreglo.h"
 #include <string>
+#include "Deporte.h"
+#include "Atleta.h"
+#include "Medicamento.h"
 #include <msclr\marshal_cppstd.h>
 
 using namespace System;
@@ -11,16 +14,25 @@ class Interfaz
 private:
 	Arreglo<Deporte> _deportes;
 	Arreglo<Atleta> _atletas;
+	Arreglo<Medicamento> _medicamentos;
 
 public:
 	Interfaz(void);
 
-	void NuevoDeporte(TextBox^ nombre);
+	void NuevoDeporte(TextBox^ TB_Nombre);
 	void CargarDeportes(DataGridView^ tabla);
 
+	void NuevoAtleta(TextBox^ TB_Nombre, TextBox^ TB_Codigo);
+	void CargarAtletas(DataGridView^ tabla);
+
+	void NuevoMedicamento(TextBox^ TB_Nombre, TextBox^ TB_Codigo, TextBox^ TB_Descripcion, CheckBox^ CB_Nocivo);
+	void CargarMedicamentos(DataGridView^ tabla);
 };
 
-Interfaz::Interfaz() { }
+Interfaz::Interfaz() {
+
+	_atletas.Tamano(0);
+}
 
 void Interfaz::NuevoDeporte(TextBox^ TB_Nombre)
 {
@@ -41,5 +53,65 @@ void Interfaz::CargarDeportes(DataGridView^ tabla)
 	{
 		string auxNombre = _deportes.Datos(i).Nombre();
 		tabla->Rows[i]->Cells[0]->Value = gcnew String(auxNombre.c_str());
+	}
+}
+
+void Interfaz::NuevoAtleta(TextBox^ TB_Nombre, TextBox^ TB_Codigo)
+{
+	string nombre = msclr::interop::marshal_as<std::string>(TB_Nombre->Text);
+	string codigo = msclr::interop::marshal_as<std::string>(TB_Codigo->Text);
+	int posNuevoAtleta = _atletas.Tamano();
+	Atleta aux;
+	aux.Nombre(nombre);
+	aux.Codigo(codigo);
+	_atletas.Datos(posNuevoAtleta,  aux);
+	_atletas.Tamano(posNuevoAtleta + 1);
+}
+
+void Interfaz::CargarAtletas(DataGridView^ tabla)
+{
+	tabla->ColumnCount = 2;
+	tabla->Columns[0]->Name = "Codigo";
+	tabla->Columns[1]->Name = "Nombre";
+	tabla->RowCount = _atletas.Tamano();
+	for(int i = 0; i < _atletas.Tamano(); ++i)
+	{
+		string auxCodigo = _atletas.Datos(i).Codigo();
+		string auxNombre = _atletas.Datos(i).Nombre();
+		tabla->Rows[i]->Cells[0]->Value = gcnew String(auxCodigo.c_str());
+		tabla->Rows[i]->Cells[1]->Value = gcnew String(auxNombre.c_str());
+	}
+}
+
+void Interfaz::NuevoMedicamento(TextBox^ TB_Nombre, TextBox^ TB_Codigo, TextBox^ TB_Descripcion, CheckBox^ CB_Nocivo)
+{
+	string nombre = msclr::interop::marshal_as<std::string>(TB_Nombre->Text);
+	string codigo = msclr::interop::marshal_as<std::string>(TB_Codigo->Text);
+	string descripcion = msclr::interop::marshal_as<std::string>(TB_Descripcion->Text);
+	bool nocivo = CB_Nocivo->Checked;
+	int posNuevoMedicamento = _medicamentos.Tamano();
+	Medicamento aux;
+	aux.Nombre(nombre);
+	aux.Codigo(codigo);
+	aux.Descripcion(descripcion);
+	aux.Nocivo(nocivo);
+	_medicamentos.Datos(posNuevoMedicamento,  aux);
+	_medicamentos.Tamano(posNuevoMedicamento + 1);
+}
+
+void Interfaz::CargarMedicamentos(DataGridView^ tabla)
+{
+	tabla->ColumnCount = 4;
+	tabla->Columns[0]->Name = "Codigo";
+	tabla->Columns[1]->Name = "Nombre";
+	tabla->Columns[2]->Name = "Descripción";
+	tabla->Columns[3]->Name = "Nocivo";
+	tabla->RowCount = _atletas.Tamano();
+	for(int i = 0; i < _atletas.Tamano(); ++i)
+	{
+		string auxCodigo = _atletas.Datos(i).Codigo();
+		string auxNombre = _atletas.Datos(i).Nombre();
+		tabla->Rows[i]->Cells[0]->Value = gcnew String(auxCodigo.c_str());
+		tabla->Rows[i]->Cells[1]->Value = gcnew String(auxNombre.c_str());
 	}
 }
